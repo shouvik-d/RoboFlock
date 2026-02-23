9e44b47c4815483c0317fb7d19e7e2bbb30f9e15
Adityasc9 <116690197+Adityasc9@users.noreply.github.com>
2026-02-23 12:34:53 -0500

    test on shouvik

--- URDF_description/launch/gazebo.launch.py
+++ URDF_description/launch/gazebo.launch.py
@@ -1,73 +1,79 @@
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
 import os
-import xacro
 from ament_index_python.packages import get_package_share_directory
-
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
+import xacro
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('URDF_description')
+    pkg_name = 'URDF_description'
+    pkg_share = get_package_share_directory(pkg_name)
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'URDF.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_urdf = robot_description_config.toxml()
+    # Process URDF file
+    xacro_file = os.path.join(pkg_share, 'urdf', 'URDF.xacro')
+    doc = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': doc.toxml()}
 
-    robot_state_publisher_node = Node(
+    # Robot State Publisher
+    node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher',
-        parameters=[
-            {'robot_description': robot_urdf}
-        ]
+        output='screen',
+        parameters=[robot_description, {"use_sim_time": True}]
     )
 
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher'
+    # Gazebo
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
     )
 
-    gazebo_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzserver.launch.py'
-            ])
-        ]),
-        launch_arguments={
-            'pause': 'true'
-        }.items()
-    )
-
-    gazebo_client = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzclient.launch.py'
-            ])
-        ])
-    )
-
-    urdf_spawn_node = Node(
+    # Spawn Robot
+    spawn_entity = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=[
-            '-entity', 'URDF',
-            '-topic', 'robot_description'
-        ],
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
         output='screen'
     )
 
+    # Controllers
+    load_joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    load_diff_drive_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller"],
+    )
+
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        parameters=[{"use_sim_time": True}]
+    )
+
     return LaunchDescription([
-        robot_state_publisher_node,
-        joint_state_publisher_node,
-        gazebo_server,
-        gazebo_client,
-        urdf_spawn_node,
+        node_robot_state_publisher,
+        gazebo,
+        spawn_entity,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_diff_drive_controller],
+            )
+        ),
+        rviz
     ])
